c9c64da81cdab91f1c6380324a18aedbc5d7b888
Early <afiazearly@gmail.com>
2026-01-28 19:58:18 +0600

    multi line comment  problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -15,9 +15,10 @@ registerMember(20); //Line B
 return 0;
 }
 
-// 1) Line A will give error first,compiler stops at line A.
-// 2) The ambiguity we saw in the given program was  because it was against function overloading rules.
-      // In the problem both function name was same also their datatype of variable(id). 
-      //So i changed the datatype of variable of first funcion which passes the valuse provided in the main function(line B) to 
-      //ignore ambiguity.
+/* 1) Line A will give error first,compiler stops at line A.
+   2) The ambiguity we saw in the given program was  because it was against function overloading rules.
+   In the problem both function name was same also their datatype of variable(id). 
+   So I changed the datatype of variable of first funcion which passes the valuse provided in the main function(line B) to 
+   ignore ambiguity. */
+
     
